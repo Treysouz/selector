c0aeb52b6bf76f6a161953181e3f8abfeee7be6a
Tremayne Souza <treysouz@gmail.com>
2020-02-18 20:46:48 -0500

    Add shape selection functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Shape from "./shape.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Selector extends Component {
+  constructor(){
+    super();
+    this.state = {
+      selectedShape: "Nothing",
+    }
+  }
+
+  selectShape = (shapeName) =>{
+    this.setState({
+      selectedShape: shapeName,
+    })
+  }
+  render() {
+    return (
+      <main className="container">
+        <section>
+          <div className="selectedBar">
+            <p>
+              You Selected: <span>{this.state.selectedShape}</span>
+            </p>
+          </div>
+        </section>
+        <section className="shapeList">
+          <Shape shape="circle" selectShape={this.selectShape}/>
+          <Shape shape="triangle" selectShape={this.selectShape}/>
+          <Shape shape="square" selectShape={this.selectShape}/>
+          <Shape shape="star" selectShape={this.selectShape}/>
+        </section>
+      </main>
+    );
+  }
 }
 
-export default App;
+export default Selector;
